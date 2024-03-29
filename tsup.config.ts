4c4398f4fbd4743421a3b121704a8af3267951b5
jozefini <jozibashaj@gmail.com>
2024-03-29 23:54:58 +0100

    init

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: {
     index: 'src/index.tsx',
     store: 'src/store.tsx',
-    hooks: 'src/hooks.tsx',
+    hooks: 'src/loop.tsx',
   },
   splitting: true,
   sourcemap: true,
